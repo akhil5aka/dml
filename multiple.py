34a2e263a345185324dc1c559b09b09db37409ef
akhil5aka <79839305+akhil5aka@users.noreply.github.com>
2022-03-25 15:22:39 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+figure,axis= plt.subplots(2,2)
+x1=[10,20,30]
+y1=[10,20,30]
+axis[0,0].plot(x1,y1)
+axis[0,0].set_title("plot 1")
+x2=[10,10,10]
+y2=[30,40,50]
+axis[0,1].plot(x2,y2)
+axis[0,1].set_title("plot 2")
+plt.show()
